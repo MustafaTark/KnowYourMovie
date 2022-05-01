f1929c431ecb5ac98dd5c1733239e5b92cd546cc
Mostafa Tarek <Mostafa_201900820@fci.helwan.edu.eg>
2022-05-01 17:11:29 +0200

    finshed

---
+++ IMDB2/Data Handleing/Upload.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace IMDB2.Data
+{
+    public class Upload
+    {
+        public static byte[] UploadImageInDataBase(HttpPostedFileBase file)
+        {
+            //file.InputStream.Read(movieInDb.Image, 0, file.ContentLength);
+            var stream = file.InputStream;
+            var reader = new BinaryReader(stream);
+            byte[] imageBytes = reader.ReadBytes(file.ContentLength);
+            return imageBytes;
+        }
+    }
+}
